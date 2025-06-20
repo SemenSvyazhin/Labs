f78f093944deda0d3afaa3fa7932aed0c7212679
Uta4kaKrya <146760246+SemenSvyazhin@users.noreply.github.com>
2025-06-20 19:51:50 +0500

    Update Ex1.java

--- src/lab11/Ex1.java
+++ src/lab11/Ex1.java
@@ -1,4 +1,19 @@
 package lab11;
 
+import java.util.Arrays;
+
 public class Ex1 {
+    public static int[] filterEvenNumbers(int[] numbers) {
+        return Arrays.stream(numbers)
+                .filter(n -> n % 2 == 0)
+                .toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] result = filterEvenNumbers(input);
+
+        System.out.println("Исходный массив: " + Arrays.toString(input));
+        System.out.println("Массив с четными числами: " + Arrays.toString(result));
+    }
 }
