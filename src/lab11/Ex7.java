3571a257a9747c4388ae4bbf006300b9798bbc40
Uta4kaKrya <146760246+SemenSvyazhin@users.noreply.github.com>
2025-06-20 19:53:18 +0500

    Update Ex7.java

--- src/lab11/Ex7.java
+++ src/lab11/Ex7.java
@@ -1,4 +1,22 @@
 package lab11;
 
-public class Ex2 {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ex7 {
+    public static List<String> filterStringsByLength(List<String> strings, int minLength) {
+        return strings.stream()
+                .filter(s -> s.length() > minLength)  // Фильтруем по длине
+                .collect(Collectors.toList());         // Собираем в новый список
+    }
+
+    public static void main(String[] args) {
+        List<String> words = List.of("Java", "Stream", "API", "Programming", "Code");
+        int minLength = 4;
+
+        List<String> longWords = filterStringsByLength(words, minLength);
+
+        System.out.println("Исходный список: " + words);
+        System.out.println("Строки длиннее " + minLength + " символов: " + longWords);
+    }
 }
