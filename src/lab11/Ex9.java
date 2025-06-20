29140b6d98b0c7ac5654065d6dcd7bee205a3875
Uta4kaKrya <146760246+SemenSvyazhin@users.noreply.github.com>
2025-06-20 19:53:52 +0500

    Update Ex9.java

--- src/lab11/Ex9.java
+++ src/lab11/Ex9.java
@@ -1,4 +1,20 @@
 package lab11;
 
-public class Ex2 {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ex9 {
+    public static List<String> filterLettersOnly(List<String> strings) {
+        return strings.stream()
+                .filter(s -> !s.isEmpty() && s.chars().allMatch(Character::isLetter))
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<String> input = List.of("Hello", "Java8", "StreamAPI", "2023", "OnlyLetters", "!@#", "");
+        List<String> result = filterLettersOnly(input);
+
+        System.out.println("Исходный список: " + input);
+        System.out.println("Строки только с буквами: " + result);
+    }
 }
